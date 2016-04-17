8b2df3df91df46850217c4c33ab0d54e874a8111
etmckay1 <ericmckay4@gmail.com>
2016-04-17 03:48:54 +0000

    remove interview question solutions

--- lib/interview_questions.rb
+++ lib/interview_questions.rb
@@ -7,49 +7,4 @@ class Interview
     # def self.factorial_loop(n)
     #     ....
     # end
-
-    def self.factorial_loop(n)
-        factorial = 1
-        i = 2
-        while i <= n
-            factorial = factorial * i
-            i += 1
-        end
-        return factorial
-    end
-
-    def self.factorial_recursion(n)
-        return 1 if n == 1
-        return factorial_recursion(n-1) * n
-    end
-
-
-    def self.fibonacci_loop(n)
-        return 1 if n == 0
-        return 1 if n == 1
-        prev_number = 1
-        prev_prev_number = 1
-        next_number = 2
-        (2..n).each do |i|
-            next_number = prev_number + prev_prev_number
-            prev_prev_number = prev_number
-            prev_number = next_number
-        end
-        return next_number
-    end
-
-    def self.fibonacci_recursion(n, n0=1, n1=1)
-        return n0 if n == 0
-        return n1 if n == 1
-        return fibonacci_recursion(n-1, n0, n1) + fibonacci_recursion(n-2, n0, n1)
-    end
-
-    def self.ways_to_make_change(n, coin_values)
-        return 0 if n < 0
-        return 0 if coin_values.count == 0
-        return 1 if n == 0
-
-        return ways_to_make_change(n - coin_values.last, coin_values) +
-                    ways_to_make_change(n, coin_values.first(coin_values.count-1))
-    end
 end
